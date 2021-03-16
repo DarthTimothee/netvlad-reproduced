47309aef1660d251685c73b851fbe60f247f41fa
DarthTimothee <timoverlaan@gmail.com>
2021-03-16 19:09:33 +0100

    Setup base structure for the network

---
+++ NetVladLayer.py
@@ -0,0 +1,54 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from VladCore import VladCore
+
+
+class NetVladLayer(nn.Module):
+
+    def __init__(self, w, b, K):
+        # TODO: where to get amount of kernels? hyper parameter?
+        super(NetVladLayer, self).__init__()
+
+        self.K = K
+
+        # TODO: kernel size, used w, b now (because it says something like that in figure 2.
+        # TODO: padding based on kernel size
+        # TODO: out channels, set them equal to K now to get vladCore to work, is that correct?
+        self.conv = nn.Conv2d(in_channels=256, out_channels=K, kernel_size=(w, b))
+        self.vlad_core = VladCore(K)
+        self.intra_normalization = nn.Identity()   # TODO: normalization
+        self.L2 = nn.Identity()  # TODO: normalization
+
+    def forward(self, x):
+        """
+        input: x <- (W x H x D) map interpreted as N x D local descriptors x
+        output:         (K x D) x 1, VLAD vector TODO: although now the 1 x dimension is not there
+        """
+
+        a_bar = F.softmax(self.conv(x))
+        # TODO: a_bar calculation. I don't think what we have now is the same as in the paper?
+
+        # x <- (1 x N x H x W)
+        x = x.flatten(0, 1).flatten(1, 2)  # interpret as N x D local descriptors
+        # x <- (N x D)
+
+        V = self.vlad_core(x, a_bar).T
+        print(f"V.shape: {V.shape}, expected (K x D) = ({self.K}, {x.shape[0]})")
+
+        K = V.shape[0]
+        D = V.shape[1]
+
+        # NOTE: intra_normalization should be column-wise. Assumed K are the columns here
+
+        # Intra normalization (column-wise L2 normalization):
+        V = F.normalize(V, p=2, dim=0)
+
+        # L2 normalization
+        y = F.normalize(V, p=2)
+
+        # TODO: double check the normalization here, because I don't trust it
+
+        return y
+
+    def backward(self, dx):
+        return dx  # TODO: backward pass
---
+++ pitts_250k_test.py
@@ -0,0 +1,57 @@
+import torch.nn as nn
+import torch
+from PIL import Image
+from torchvision import transforms, models
+import matplotlib.pyplot as plt
+
+
+from NetVladCNN import NetVladCNN
+
+
+def alex_forward(self, x: torch.Tensor) -> torch.Tensor:
+    return self.features(x)
+
+
+# Hyper parameters, based on the appendix
+K = 64  # amount of kernels
+m = 0.1  # margin for the loss
+lr = 0.001  # or 0.0001 depending on the experiment, which is halved every 5 epochs
+momentum = 0.9
+wd = 0.001
+batch_size = 4  # batch size is 4 tuples
+max_epochs = 30  # but usually convergence occurs much faster
+
+
+# Setup base network
+alex_base = torch.hub.load('pytorch/vision:v0.6.0', 'alexnet', pretrained=True)
+alex_base.features = alex_base.features[:11]  # cut off before last ReLu
+alex_base.avgpool = nn.Identity()  # bypass avg_pool and classifier
+alex_base.classifier = nn.Identity()  # bypass avg_pool and classifier
+alex_base.forward = type(alex_base.forward)(alex_forward, alex_base)
+
+# Setup our network
+net = NetVladCNN(base_cnn=alex_base, K=K)
+
+# Import an image
+filename = "data/000000_pitch1_yaw1.jpg"
+input_image = Image.open(filename)
+
+# Show the image
+# plt.figure()
+# plt.title(f"example image: {filename}")
+# plt.imshow(input_image)
+# plt.show()
+
+# Preprocess the image
+preprocess = transforms.Compose([
+    transforms.Grayscale(num_output_channels=3),
+    transforms.Resize(256),  # TODO: resize/crop?
+    transforms.CenterCrop(256),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # TODO: keep these normalize constants?
+])
+input_tensor = preprocess(input_image)
+x = input_tensor.unsqueeze(0)  # alex_net wants a batch!
+
+y = net.forward(x)
+print(f"y.size() = {y.size()}")
