50107523f49d7bcfc79c5184f02eaf9bf57d0479
ElwinDuinkerken <33690970+ElwinDuinkerken@users.noreply.github.com>
2021-03-18 15:46:43 +0100

    Add datasets + test

---
+++ database.py
@@ -0,0 +1,18 @@
+import scipy.io
+
+
+db = scipy.io.loadmat('./datasets/pitts250k_train.mat')
+
+
+def get_image_name(image_id):
+    return db.get('dbStruct')[0][0][1][image_id][0][0]
+
+
+def get_position(image_id):
+    x = db.get('dbStruct')[0][0][2][0][image_id]
+    y = db.get('dbStruct')[0][0][2][1][image_id]
+    return x, y
+
+
+print(get_image_name(5))
+print(get_position(5))
