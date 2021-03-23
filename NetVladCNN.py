e49d7cb3bdc2440caeaf1faddb49e9d6b35482cd
DarthTimothee <timoverlaan@gmail.com>
2021-03-23 20:35:08 +0100

    Implement most of batching

--- NetVladCNN.py
+++ NetVladCNN.py
@@ -16,7 +16,13 @@ class NetVladCNN(torch.nn.Module):
         self.netvlad_layer = NetVladLayer(K=K, D=self.D)
 
     def forward(self, x):
-        # TODO: make it batch proof, right now it can handle only batches of size 1
+        """
+        inputs:
+            x <- (batch_size, 3, H_pixels, W_pixels) input image tensor
+        output:
+                 (K, N, batch_size)
+        """
+
         feature_map = self.base_cnn(x)
         # feature_map is now a (D x N) tensor
         return self.netvlad_layer(feature_map, self.c)
@@ -31,39 +37,38 @@ class NetVladLayer(nn.Module):
         super(NetVladLayer, self).__init__()
         self.K = K
         self.D = D
-
-        # TODO: padding based on kernel size
         self.conv = nn.Conv2d(in_channels=D, out_channels=K, kernel_size=(1, 1), bias=True)  # TODO: bias?
         self.vlad_core = VladCore(K, D)
 
     def forward(self, x, c):
         """
-        input: x <- (1 x D x H x W) map interpreted as N x D local descriptors x
-        output:             (K x D) x 1, VLAD vector TODO: although now the 1 x dimension is not there
+        input: x <- (batch_size x D x H x W) map interpreted as N x D local descriptors x
+        output:     (K x D) x batch_size, VLAD vectors
         """
         K, D = self.K, self.D
         H, W = x.shape[2:]
+        batch_size = x.shape[0]
         N = W * H
-        assert x.shape == (1, D, H, W)
+        assert x.shape == (batch_size, D, H, W)
 
         # Soft assignment
         features = self.conv(x)
-        assert features.shape == (1, K, H, W)
+        assert features.shape == (batch_size, K, H, W)
 
-        a_bar = F.softmax(features, dim=1)
-        assert a_bar.shape == (1, K, H, W)
+        a_bar = F.softmax(features, dim=2)
+        assert a_bar.shape == (batch_size, K, H, W)
 
-        x = x.view(D, N)  # "interpret as N x D local descriptors"
-        a_bar = a_bar.view(K, N)
+        x = x.view(batch_size, D, N)  # "interpret as N x D local descriptors"
+        a_bar = a_bar.view(batch_size, K, N)
 
         # VLAD vector
         V = self.vlad_core(x, a_bar, c)
-        assert V.shape == (K, D)
+        assert V.shape == (D, K, batch_size)
 
         # TODO: double check the normalization here, because I don't trust it
         # NOTE: intra_normalization should be column-wise. Assumed K are the columns here
         # Intra normalization (column-wise L2 normalization):
-        V = F.normalize(V, p=2, dim=0)
+        V = F.normalize(V, p=2, dim=1)
 
         # L2 normalization
         y = F.normalize(V, p=2)
@@ -83,20 +88,29 @@ class VladCore(nn.Module):
         input:  x     <- (D x N)
                 a_bar <- (1 x K x H x W)  -> moet iig N en K hebben voor equation 1
                 c     <- TODO: I think c is also a parameter? what dimensions does it have?
-        output: V     <- (D x K)
+        output: V     <- (D x K) x batch_size
         """
-        K, D, N = self.K, self.D, x.shape[1]
-        assert x.shape == (D, N)
-        assert a_bar.shape == (K, N)
+        K, D, N, batch_size = self.K, self.D, x.shape[2], x.shape[0]
+        assert x.shape == (batch_size, D, N)
+        assert a_bar.shape == (batch_size, K, N)
 
-        V = torch.zeros((K, D))
+        V = torch.zeros((batch_size, K, D))
         x_numpy = x.detach().numpy()
-        #with torch.no_grad():  # TODO: this fixes an error so we don't need to go to numpy, but will this cause issues when optimizing?
-        for k in range(K):
-            asdf = (x_numpy.T - c[k]).T
-            V[k] = torch.mv(torch.tensor(asdf), a_bar[k].double())
+        # with torch.no_grad():  # TODO: this fixes an error so we don't need to go to numpy, but will this cause issues when optimizing?
+        for b in range(batch_size):
+            for k in range(K):
+                # print(f"x_numpy.T.shape: {x_numpy.T.shape}")
+                # print(f"c.shape: {c.shape}")
+                A = torch.tensor(x_numpy.T - c[None, k][:, :, None])  # TODO: probably better syntax for this?
+                B = a_bar[:, k].double()
+                # print(f"A.shape: {A.shape}")
+                # print(f"B.shape: {B.shape}")
+                Y = torch.tensordot(A[:, :, b], B[b, :], dims=([0], [0]))
+                # print(f"Y.shape: {Y.shape}")
 
-        return V
+                V[b, k] = Y
+
+        return V.T
 
 
 class AlexBase(nn.Module):
